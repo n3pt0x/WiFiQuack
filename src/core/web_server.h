21803cc5ad5089f597f0d1b26c44ee3cd924b02a
n3pt0x <157751271+n3pt0x@users.noreply.github.com>
2026-06-23 22:33:18 +0200

    feat: defined route, request sender, page in PROGMEM + GZIP

--- src/core/web_server.h
+++ src/core/web_server.h
@@ -4,7 +4,14 @@
 #include <WebServer.h>
 
 const int WEB_SERVER_PORT = 80;
+extern WebServer server;
 
-void runWebServer();
+void initWebServer();
+void initRoutes();
+void handleIndex();
+void handleJS();
+void handleCSS();
+void handleDuckyScript();
+void handle404();
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,29 @@
 #include <Arduino.h>
 #include "core/config.h"
 #include "core/wifi_manager.h"
+#include "core/web_server.h"
+#include "core/utils.h"
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
   delay(1000);
 
+  printBanner();
+
   bool wifiOk = startWiFiAP();
   if (!wifiOk) {
+        unsigned long lastWiFiCheck = millis();
         Serial.println("Creating AP failed !");
         while (true) {
           delay(1000);
         }
-    } else {
+  } else {
         Serial.println("AP successfully created !");
         printWiFiInfos();
-    }
   }
+  initWebServer();
+}
 
 void loop() {
-  handleWiFiClients();
-  delay(5000);
+  server.handleClient();
 }
