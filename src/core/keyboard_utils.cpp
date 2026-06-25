d11414de6e6be23df41a138eaf975ce94072c78e
n3pt0x <157751271+n3pt0x@users.noreply.github.com>
2026-06-25 21:48:45 +0200

    revert: remove accenntTable

--- src/core/keyboard_utils.cpp
+++ src/core/keyboard_utils.cpp
@@ -12,8 +12,10 @@ namespace keyboard_utils {
                 Keyboard.begin(KeyboardLayout_fr_FR);
                 break;
             case LAYOUT_US:
-            default:
                 Keyboard.begin(KeyboardLayout_en_US);
+                break;
+            default:
+                Keyboard.begin(KeyboardLayout_fr_FR);
                 break;
         }
     }
@@ -29,15 +31,7 @@ namespace keyboard_utils {
         Keyboard.releaseAll();
     }
 
-    void sendChar(const char* str) {
-        uint8_t key = 0;
-        uint8_t modifier = 0;
-
-        for (size_t accent = 0; accent < sizeof(accentTable); accent++) {
-            if (strcmp(str, accentTable[accent].utf8)) {
-                Keyboard.write(accentTable[accent].keyCode);
-            }
-        }
-        
+    void sendString(const String& str) {
+        Keyboard.print(str);
     }
 }
