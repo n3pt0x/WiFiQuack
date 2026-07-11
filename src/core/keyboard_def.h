6503437737069da8e65945d3619a977f46624ae0
n3pt0x <157751271+n3pt0x@users.noreply.github.com>
2026-07-11 16:30:33 +0200

    fix: remove not use KEY_CODE

--- src/core/keyboard_def.h
+++ src/core/keyboard_def.h
@@ -3,17 +3,6 @@
 #ifdef PLATFORM_ESP32
     #include <USB.h>
     #include <USBHIDKeyboard.h>
-    #ifndef KEY_POWER
-    #define KEY_POWER 0x66
-    #endif
-    
-    #ifndef KEY_SLEEP
-    #define KEY_SLEEP 0x82
-    #endif
-    
-    #ifndef KEY_RESET
-    #define KEY_RESET 0x9A
-    #endif
 #elif defined(PLATFORM_PICO)
     #include <Keyboard.h>
     #include <HID_Keyboard.h>
