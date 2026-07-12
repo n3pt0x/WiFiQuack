0eabed2a0a70cf06714f186733a367bfdcbd04f0
n3pt0x <157751271+n3pt0x@users.noreply.github.com>
2026-07-12 22:34:27 +0200

    feat(clipboard): add clipboard

--- web/editor.js
+++ web/editor.js
@@ -1,4 +1,4 @@
-import { run, download } from "./main.js"
+import { run, download } from "./main.js";
 
 document.addEventListener("keydown", function (e) {
   handleShortCut(e, "Enter", function () {
@@ -12,9 +12,20 @@ document.addEventListener("keydown", function (e) {
   });
 });
 
+document.getElementById("copyBtn").addEventListener("click", function (e) {
+  clipboard();
+});
+
 function handleShortCut(event, key, fn) {
   if (event.ctrlKey && event.key == key) {
     event.preventDefault();
     fn();
   }
+}
+
+function clipboard() {
+  const payload = document.getElementById("payload");
+  payload.select();
+  document.execCommand("copy");
+  payload.setSelectionRange(0, 0);
 }
