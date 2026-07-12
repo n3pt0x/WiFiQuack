b6cb2e09aeb1e896abf63d11277b089e30939723
n3pt0x <157751271+n3pt0x@users.noreply.github.com>
2026-07-12 19:33:08 +0200

    feat(upload): Adding script upload

--- web/main.js
+++ web/main.js
@@ -1,36 +1,107 @@
-document.getElementById("duckyForm").addEventListener("submit", async function(e) {
-    e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("duckyForm")
+    .addEventListener("submit", async function (e) {
+      e.preventDefault();
+      run();
+    });
 
-    const payload = document.getElementById("payload").value;
-    const status = document.getElementById("status");
-    
-    status.textContent = "Payload sending ...";
+  document.getElementById("download").addEventListener("click", function () {
+    download();
+  });
 
-    try {
-        const response = await fetch('/run', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-                },
-            body: "script=" + encodeURIComponent(payload)
-        });    
-        const result = await response.text();
-        status.textContent = `${result}`;
-    } catch (error) {
-        status.textContent = `Error: ${error.message}`;
-    }
+  document.getElementById("upload").addEventListener("click", function () {
+    upload();
+  });
 });
 
-document.getElementById("download").addEventListener("click", function(e) {
+function download() {
+  const payload = document.getElementById("payload").value;
+  const status = document.getElementById("status");
+
+  if (payload != "") {
     const filename = prompt("Enter filename", "payload.bin");
 
     if (filename) {
-        var element = document.createElement('a');
-        element.setAttribute('href', 'data:text/plain;charset=utf_8,' + encodeURIComponent(payload));
-        element.setAttribute('download', filename);
-        element.style.display = 'none';
-        document.body.appendChild(element);
-        element.click();
-        document.body.removeChild(element);
+      var element = document.createElement("a");
+      element.setAttribute(
+        "href",
+        "data:text/plain;charset=utf_8," + encodeURIComponent(payload),
+      );
+      element.setAttribute("download", filename);
+      element.style.display = "none";
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+      status.textContent = "File downloaded correctly";
     }
-});
+  } else {
+    status.textContent = "Cannot download an empty file";
+  }
+}
+
+async function run() {
+  const payload = document.getElementById("payload").value;
+  const status = document.getElementById("status");
+
+  status.textContent = "Payload sending ...";
+
+  try {
+    const response = await fetch("/run", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: "script=" + encodeURIComponent(payload),
+    });
+    const result = await response.text();
+    status.textContent = `${result}`;
+  } catch (error) {
+    status.textContent = `Error: ${error.message}`;
+  }
+}
+
+function upload() {
+  const fileInput = document.createElement("input");
+  fileInput.type = "file";
+  fileInput.style.display = "none";
+  document.body.appendChild(fileInput);
+
+  fileInput.click();
+
+  fileInput.addEventListener("change", function (e) {
+    const payload = document.getElementById("payload");
+    const status = document.getElementById("status");
+
+    const file = e.target.files[0];
+
+    if (!file) {
+      console.error("Select a file please");
+    }
+
+    status.textContent = `File: ${file.name} (${file.size} bytes) has been uploaded`;
+
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      try {
+        const content = event.target.result;
+        payload.value = content;
+        fileInput.remove();
+      } catch (error) {
+        status.textContent = `Error: ${error.message}`;
+        console.error("Error: " + error.message);
+      }
+      reader.error = function () {
+        status.textContent = `Error: ${error.message}`;
+        console.error("Error: " + error.message);
+      };
+    };
+    reader.readAsText(file);
+    fileInput.value = "";
+    fileInput.remove();
+  });
+
+  fileInput.addEventListener("cancel", function () {
+    fileInput.remove();
+  });
+}
