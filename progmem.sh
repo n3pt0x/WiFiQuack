81d8aecce6db83c767cf706a4048a74b2b8d935e
n3pt0x <157751271+n3pt0x@users.noreply.github.com>
2026-07-21 09:18:14 +0200

    feat: minify js files

--- progmem.sh
+++ progmem.sh
@@ -5,12 +5,36 @@ declare -u name
 declare -a var_names=()
 declare -a var_data=()
 
-for file in web/*; do
+tmp_path="$(mktemp -d)"
+
+cp web/* $tmp_path
+
+if ! [[ -d venv ]]; then
+    pip_install="Y"
+    read -p "Python package jsmin not founded. Do you want install it ? [Y/n]" pip_install
+    if [[ "$pip_install" =~ ^[Yy]$ ]]; then
+        python3 -m venv venv
+        source venv/bin/activate
+        pip install jsmin
+    else
+        echo "jsmin is require !"
+        exit 1;
+    fi
+else
+    source venv/bin/activate
+fi
+
+for file in $tmp_path/*; do
     if [[ -f "$file" ]]; then
         basename="$(basename "$file")"
+        extension="${file##*.}"
         name="PAGE_${basename//./_}_GZ[]"
-        
-        gz_data=$(gzip -9 -c $file | xxd -i | tr -d '\n' | sed 's/}/}\n/g'; echo -e "\n")
+
+        if [[ "$extension" == "js" ]]; then
+            python3 -m jsmin "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
+        fi
+
+        gz_data=$(gzip -9 -c $file | xxd -i | tr -d '\n' | sed 's/}/}\n/g'; printf "\n")
             
         var_names+=("$name")    
         var_data+=("$gz_data")
@@ -26,3 +50,7 @@ EOF
 for i in "${!var_names[@]}"; do
     echo "const char ${var_names[$i]} PROGMEM = {${var_data[$i]}};" >> src/core/webfiles.h
 done
+
+if [[ -d "$tmp_path" ]]; then
+    rm -rf $tmp_path
+fi
