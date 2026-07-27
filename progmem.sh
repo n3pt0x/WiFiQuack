d6561994f0527399404ab21c822e0632a2020bd5
n3pt0x <157751271+n3pt0x@users.noreply.github.com>
2026-07-27 23:19:07 +0200

    fix: remove js minification

--- progmem.sh
+++ progmem.sh
@@ -9,30 +9,11 @@ tmp_path="$(mktemp -d)"
 
 cp web/* $tmp_path
 
-if ! [[ -d venv ]]; then
-    pip_install="Y"
-    read -p "Python package jsmin not founded. Do you want install it ? [Y/n]" pip_install
-    if [[ "$pip_install" =~ ^[Yy]$ ]]; then
-        python3 -m venv venv
-        source venv/bin/activate
-        pip install jsmin
-    else
-        echo "jsmin is require !"
-        exit 1;
-    fi
-else
-    source venv/bin/activate
-fi
-
 for file in $tmp_path/*; do
     if [[ -f "$file" ]]; then
         basename="$(basename "$file")"
         extension="${file##*.}"
         name="PAGE_${basename//./_}_GZ[]"
-
-        if [[ "$extension" == "js" ]]; then
-            python3 -m jsmin "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
-        fi
 
         gz_data=$(gzip -9 -c $file | xxd -i | tr -d '\n' | sed 's/}/}\n/g'; printf "\n")
             
