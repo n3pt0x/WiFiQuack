375fc1076df20f51d9c7df525be9182013a6c3e0
n3pt0x <157751271+n3pt0x@users.noreply.github.com>
2026-07-31 08:34:21 +0200

    update: set fix IP and disable log if not DEBUG mode

--- src/core/wifi_manager.cpp
+++ src/core/wifi_manager.cpp
@@ -3,10 +3,17 @@
 #include "settings.h"
 #include "debug.h"
 
-bool startWiFiAP()
-{
+bool startWiFiAP() {
+    #ifndef DEBUG
+        esp_log_level_set("wifi", ESP_LOG_NONE);
+        esp_log_level_set("dhcps", ESP_LOG_NONE);
+        esp_log_level_set("dhcpc", ESP_LOG_NONE);
+    #endif
+    
+    IPAddress ip (192, 168, 4, 1);
     WiFi.mode(WIFI_AP);
-    return WiFi.softAP(settings::wifi_ssid, settings::wifi_passphrase);
+    
+    return WiFi.softAP(settings::wifi_ssid.c_str(), settings::wifi_passphrase.c_str()) && WiFi.softAPConfig(ip, ip, IPAddress(255, 255, 255, 0));
 }
 
 void printWiFiInfos() {
